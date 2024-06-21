37e60d1956e9de3d1a51f76de41cd8c702b41015
Alex Booker <booker@booker.codes>
2024-06-21 14:39:16 +0100

    404 if domain not found

--- src/app/[domain]/layout.tsx
+++ src/app/[domain]/layout.tsx
@@ -1,8 +1,12 @@
 
+import { db } from "@/db";
+import { orgsTable } from "@/schema";
+import { log } from "console";
+import { eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
 import { ReactNode } from "react";
 
-export default function Layout({
+export default async function Layout({
   params,
   children,
 }: {
@@ -10,9 +14,13 @@ export default function Layout({
   children: ReactNode;
 }) {
 
-  // if (params.domain !== "bookercodes") {
-  //   notFound()
-  // }
+  const [org] = await db.select()
+    .from(orgsTable)
+    .where(eq(orgsTable.slug, params.domain))
+
+  log('org', org)
+  if (!org)
+    notFound()
 
   return (
     <div>
