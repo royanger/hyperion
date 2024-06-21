ae713e62d0db287c269c78e7268faf409e29055e
Alex Booker <booker@booker.codes>
2024-06-21 11:31:43 +0100

    show org list

---
+++ src/app/choose-org/page.tsx
@@ -0,0 +1,12 @@
+
+import { OrganizationList, OrganizationSwitcher } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Choose org</h1>
+      <OrganizationList hidePersonal />
+    </>
+  );
+}
